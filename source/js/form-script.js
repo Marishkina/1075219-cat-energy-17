ee7de1b75b775409bd0845f250d1d0db17047421
Marina <marishkina@gmail.com>
2019-09-03 01:53:28 +0800

    fill для иконок в форме

--- source/js/form-script.js
+++ source/js/form-script.js
@@ -1,11 +1,11 @@
 var form = document.querySelector(".form");
-var name = form.querySelector(".input-field--name");
+var catName = form.querySelector(".input-field--cat-name");
 var weight = form.querySelector(".input-field--weight");
 var age = form.querySelector(".input-field--age");
 var email = form.querySelector(".input-field--email");
 var phone = form.querySelector(".input-field--phone");
-var storage = localStorage.getItem("name");
-var storage = localStorage.getItem("weight");
+var storage = localStorage.getItem("catName");
+var storage = localStorage.getItem("weightField");
 var storage = localStorage.getItem("age");
 var storage = localStorage.getItem("email");
 var storage = localStorage.getItem("phone");
@@ -13,7 +13,7 @@ var storage = localStorage.getItem("phone");
 var isStorageSupport = true;
   var storage = "";
   try {
-    storage = localStorage.getItem("name");
+    storage = localStorage.getItem("catName");
     storage = localStorage.getItem("weight");
     storage = localStorage.getItem("age");
     storage = localStorage.getItem("email");
@@ -23,13 +23,16 @@ var isStorageSupport = true;
   }
 
 form.addEventListener("submit", function (evt) {
-  if (!name.value || !age.value || !weight.value || !email.value || !phone.value) {
+  if (!catName.value || !age.value || !weight.value || !email.value || !phone.value) {
     evt.preventDefault();
-    name.classList.remove("input__error");
-    console.log("Нужно ввести логин и пароль");
+    catName.classList.add("input__error");
+    weight.classList.add("input__error");
+    age.classList.add("input__error");
+    email.classList.add("input__error");
+    phone.classList.add("input__error");
   } else {
     if (isStorageSupport) {
-      localStorage.setItem("name", login.value);
+      localStorage.setItem("catName", login.value);
       localStorage.setItem("weight", login.value);
       localStorage.setItem("age", login.value);
       localStorage.setItem("email", login.value);
